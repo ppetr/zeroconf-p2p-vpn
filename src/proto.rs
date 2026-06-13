2edc511d2837fa90c21c746a8524f43d1ee18336
Petr <petr.mvd+git@gmail.com>
2026-06-13 17:57:47 +0200

    Add methods for reading and writing `v1::Control` messages in uni-directional QUIC Iroh streams

--- src/proto.rs
+++ src/proto.rs
@@ -1,3 +1,7 @@
+use anyhow::Result;
+use iroh::endpoint::{Connection, RecvStream, VarInt};
+use prost::Message;
+
 pub mod p2p_vpn {
     pub mod control {
         pub mod v1 {
@@ -7,3 +11,28 @@ pub mod p2p_vpn {
 }
 
 use p2p_vpn::control::v1;
+
+const MAX_MESSAGE_SIZE: usize = 2048;
+
+// Wait for an unidirectional stream and accept a `v1::Control` message from it.
+pub async fn read_control(conn: &Connection) -> Result<v1::Control> {
+    let mut stream = conn.accept_uni().await?;
+    let control = read_control_stream(&mut stream).await;
+    stream.stop(/*error_code=*/VarInt::from_u32(control.as_ref().map_or(1, |_| 0)))?;
+    control
+}
+
+async fn read_control_stream(stream: &mut RecvStream) -> Result<v1::Control> {
+    let buf = stream.read_to_end(MAX_MESSAGE_SIZE).await?;
+    Ok(v1::Control::decode(buf.as_slice())?)
+}
+
+// Open an unidirectional stream and send a `v1::Control` message to it.
+pub async fn write_control(control: &v1::Control, conn: &Connection) -> Result<()> {
+    let mut buf = Vec::with_capacity(MAX_MESSAGE_SIZE);
+    control.encode_length_delimited(&mut buf)?;
+    let mut stream = conn.open_uni().await?;
+    let result = stream.write_all(&buf).await;
+    stream.finish()?;
+    Ok(result?)
+}
