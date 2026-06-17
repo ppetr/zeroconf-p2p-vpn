8eb43fa31102e4656572731954704bfff8337b54
Petr <petr.mvd+git@gmail.com>
2026-06-17 09:49:27 +0200

    Properly wait after writing a protobuf to a stream for the receiver to process it
    Otherwise the stream might get closed prematurely.
    
    Also when finishing a stream after reading, don't fail if it's closed already.

--- src/proto.rs
+++ src/proto.rs
@@ -18,23 +18,27 @@ const MAX_MESSAGE_SIZE: usize = 2048;
 pub async fn read_control(conn: &Connection) -> Result<v1::Control> {
     let mut stream = conn.accept_uni().await?;
     let control = read_control_stream(&mut stream).await;
-    stream.stop(
+    let _closed_stream_is_fine = stream.stop(
         /*error_code=*/ VarInt::from_u32(control.as_ref().map_or(1, |_| 0)),
-    )?;
+    );
     control
 }
 
 async fn read_control_stream(stream: &mut RecvStream) -> Result<v1::Control> {
     let buf = stream.read_to_end(MAX_MESSAGE_SIZE).await?;
-    Ok(v1::Control::decode(buf.as_slice())?)
+    let control = v1::Control::decode(buf.as_slice())?;
+    tracing::debug!("Read control command: {:?}", control);
+    Ok(control)
 }
 
 // Open an unidirectional stream and send a `v1::Control` message to it.
-pub async fn write_control(control: &v1::Control, conn: &Connection) -> Result<()> {
+pub async fn write_control(conn: &Connection, control: &v1::Control) -> Result<()> {
     let mut buf = Vec::with_capacity(MAX_MESSAGE_SIZE);
-    control.encode_length_delimited(&mut buf)?;
+    control.encode(&mut buf)?;
     let mut stream = conn.open_uni().await?;
     let result = stream.write_all(&buf).await;
+    tracing::debug_span!("Waiting for the peer to receive all data");
     stream.finish()?;
+    stream.stopped().await?;
     Ok(result?)
 }
