0d0f0d94ffda1590b9c323ac675c5292ed2b0962
Petr <petr.mvd+git@gmail.com>
2026-06-30 16:44:13 +0200

    Read data from a Quinn `RecvStream` with zero-coping into a `BufList`

--- src/proto.rs
+++ src/proto.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
+use buf_list::BufList;
+use bytes::Bytes;
 use iroh::endpoint::{Connection, RecvStream, VarInt};
 use prost::Message;
+use thin_status::{ErrorCode::*, ThinStatus};
 
 pub mod p2p_vpn {
     pub mod control {
@@ -25,8 +28,8 @@ pub async fn read_control(conn: &Connection) -> Result<v1::Control> {
 }
 
 async fn read_control_stream(stream: &mut RecvStream) -> Result<v1::Control> {
-    let buf = stream.read_to_end(MAX_MESSAGE_SIZE).await?;
-    let control = v1::Control::decode(buf.as_slice())?;
+    let buf_list = read_to_end(stream, MAX_MESSAGE_SIZE).await?;
+    let control = v1::Control::decode(buf_list)?;
     tracing::debug!(control = ?control, "Read control command");
     Ok(control)
 }
@@ -41,4 +44,29 @@ pub async fn write_control(conn: &Connection, control: &v1::Control) -> Result<(
     stream.finish()?;
     stream.stopped().await?;
     Ok(result?)
+}
+
+/// Reads the stream to its end and accumulates all chunks in a `BufList`.
+pub async fn read_to_end(recv: &mut RecvStream, max_bytes: usize) -> Result<BufList> {
+    let mut buffer_list = BufList::new();
+    let mut scratchpad = [Bytes::new(), Bytes::new(), Bytes::new(), Bytes::new()];
+    loop {
+        match recv.read_chunks(&mut scratchpad).await? {
+            Some(chunks_filled) => {
+                for chunk in &mut scratchpad[0..chunks_filled] {
+                    buffer_list.push_chunk(std::mem::take(chunk));
+                    if buffer_list.num_bytes() >= max_bytes {
+                        anyhow::bail!(ThinStatus::builder(OutOfRange)
+                            .message(&format!(
+                                "stream longer than the limit of {} bytes",
+                                max_bytes
+                            ))
+                            .build())
+                    }
+                }
+            }
+            None => break, // All chunks collected.
+        }
+    }
+    Ok(buffer_list)
 }
