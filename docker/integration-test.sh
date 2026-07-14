541575156bf1682e99b9c3cb8aa654efbb1426de
Petr <petr.mvd+git@gmail.com>
2026-07-14 10:01:57 +0200

    Extract the integration test into a helper function, add timeouts and use `ncat` that has more fine-tuned features

--- docker/integration-test.sh
+++ docker/integration-test.sh
@@ -16,21 +16,29 @@ setup_file() {
 }
 
 teardown_file() {
+    echo "~~~ Teardown ~~~" >&3
     docker compose down
 }
 
-@test "Send a stream of big data" {
-    echo "[1] Listening in node-alice on port 1234" >&3
-    local wc_file="$BATS_TEST_TMPDIR/wc_out"
-    docker compose exec -T node-alice bash -c 'nc -6 -l 1234 | wc --bytes' >"$wc_file" &
+send_data() {
+    set -e
+    local wc_file="$1"
+    echo "[1a] Listening in node-alice on port 1234" >&3
+    docker compose exec -T node-alice bash -c 'timeout -v 30 ncat --recv-only -6 -l 1234 </dev/null | wc --bytes' >"$wc_file" 2>&3 &
     sleep 1
 
     echo "[2] Sending data from node-bob" >&3
-    run docker compose exec -T node-bob bash -c "pv --numeric --stop-at-size --size 100M --stats -petab /dev/urandom | nc -6 -N fdbd:a6ce:654d:0:6f56:d923:de1e:5588 1234"
+    docker compose exec -T node-bob bash -c "timeout -v 30 pv --numeric --stop-at-size --size 100M --stats -petab /dev/urandom | ncat --send-only -6 fdbd:a6ce:654d:0:6f56:d923:de1e:5588 1234" >&3 2>&3
+    echo "[1b] Waiting for the listener to finish" >&3
+    wait %1
+}
+
+@test "Send a stream of big data" {
+    local wc_file="$BATS_TEST_TMPDIR/wc_out"
+    run send_data "$wc_file"
     [ "$status" -eq 0 ]
-    echo "$output" >&3
-    run wait
-    [ "$status" -eq 0 ]
-    run cat "$wc_file"
-    [ "$output" = "104857600" ]
+
+    echo -n "[3] Checking the output of 'wc': " >&3
+    cat "$wc_file" >&3
+    [ "$(<"$wc_file")" = "104857600" ]
 }
